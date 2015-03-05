36665918e88f81b5971220e3db24409dc5ca3264
Daniel Augusto <danisson10@gmail.com>
2015-03-05 19:55:27 -0300

    Suporte a latex

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 title="$(cat aulas/$1.md | head -2 | tail -1)"
 
-m4 aulas/$1.md | pandoc --smart -f markdown_strict+startnum+backtick_code_blocks+inline_code_attributes+markdown_in_html_blocks -t html --no-highlight \
+m4 aulas/$1.md | pandoc --mathjax --no-highlight --smart -f markdown_strict+startnum+backtick_code_blocks+inline_code_attributes+markdown_in_html_blocks+raw_tex+tex_math_dollars -t html \
                | prepend "define(\`NUM',$2)define(\`TITLE',$title)include(index.template)" | m4 > html/$1.html
